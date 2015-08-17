6b2c4102f2d99f83407b9972409169d9f75c4caa
Corne Oppelaar <eater@eaterofbsd.fritz.box>
2015-08-18 00:16:53 +0200

    start anidb title import

---
+++ public/fill-db.php
@@ -0,0 +1,5 @@
+<?php
+
+include '../vendor/autoload.php';
+
+(new Odango\AniDbTitles())->fillDatabase();
--- src/AniDbTitles.php
+++ src/AniDbTitles.php
@@ -2,10 +2,26 @@
 
 namespace Odango;
 
+use \Curl\Curl;
+
 class AniDbTitles {
+  protected $titleDumpUrl = 'http://anidb.net/api/anime-titles.xml.gz';
+
   public function fillDatabase()
   {
+    $tmpname = '/tmp/odango.php.cache';
 
+    if (!file_exists($tmpname)) {
+      $curl = new Curl();
+      $curl->setOpt(CURLOPT_ENCODING, "gzip");
+      $curl->download($this->titleDumpUrl, $tmpname);
+      $curl->close();
+    }
+
+    $xml = simplexml_load_file($tmpname);
+    foreach ($xml as $child) {
+      var_dump($child);
+    }
   }
 
   public function getAlternativeTitles($title)
